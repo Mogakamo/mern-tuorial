240d530677ceeac5403bd8b71175e74016ddec3e
Mogakamo <mogaka.amo254@gmail.com>
2021-12-28 19:21:51 +0300

    server is set

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-const 
+const express = require('express')
+
+const app = express()
+
+app.get("/", (req, res) => res.send("Hello World!"))
+
+const port = process.env.PORT || 8081;
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
