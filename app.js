61aa54e56b5c272d4c53733468a07bf5234c7b5a
Mogakamo <mogaka.amo254@gmail.com>
2021-12-29 11:07:35 +0300

    commit

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const connectDB = require('./config/db')
+var cors = require('cors');
+
+const books = require('./routes/api/books');
 
 const app = express()
 
 // Connect database
 connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }))
+
+// Init middleware
+app.use(express.json({extended: false}))
+
+app.use('api/books', books)
 
 app.get("/", (req, res) => res.send("Hello World!"))
 
